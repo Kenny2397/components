823a47a95c74c8fd8c0476bd74133a177afa6a19
kenny <kenny.luque.t@uni.pe>
2022-04-30 19:10:43 -0500

    children

--- src/App.js
+++ src/App.js
@@ -7,10 +7,23 @@ function App() {
     <div className="container">
       
       {/* <LearnReact/> */}
-      <Person name="kenny" lastName="Luque" city="Lima" age={24} hairColor="black"/>
-      <Person name="Paul" lastName="Luque" city="Lima" age={20} hairColor="brown"/>
-      <Person name="jose" lastName="fino" city="Santiago" age={34} hairColor="white"/>
+      {/* 2)hay diferencia en usar {} en los props string? */}
+
+      <Person name={"kenny"} lastName={"Luque"} city={"Lima"} age={24} hairColor={"black"}/>
+      
+      <Person name={"Paul"} lastName={"Luque"} city={"Lima"} age={20} hairColor={"brown"}/>
+      
+      <Person name="jose" lastName="fino" city="Santiago" age={34} hairColor="white">
+      <h4>this is a child2</h4>
+      <h3>oli iam another child</h3>
+      </Person>
+
       <Person name="Pablo" lastName="Marmoñl" city="Caracas" age={23} hairColor="rebbecapurple"/>
+      
+      <Person>
+        <h4>this is a child</h4>
+      </Person>
+
     </div>
     
   );
--- src/components/Person/Person.js
+++ src/components/Person/Person.js
@@ -1,13 +1,16 @@
 import './Person.css';
-
-const Person = ({name,lastName,city, age, hairColor}) => {
+// import React, {component} from 'react';
+// para acceder a los hijos se debe pasar en la funcion la palabra {children}
+const Person = ({name,lastName,city, age, hairColor, children}) => {
     return (
         <div className="main">
             <h1>{name}, {lastName}</h1>
             <h3>city: {city}</h3>
             <h3>age: {age}</h3>
             <h3>hair-color: {hairColor}</h3>
+            {children}
         </div>
+
     )
 }
 export default Person;
